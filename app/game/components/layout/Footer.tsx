7438d3681a2ea139b625f8690d08275d2782f93d
tweezerchef <ltomblock@gmail.com>
2024-05-06 21:06:58 -0500

    header, footer, and no scroll working

---
+++ app/game/components/layout/Footer.tsx
@@ -0,0 +1,34 @@
+import { Container, Group, Anchor } from "@mantine/core";
+import Image from "next/image";
+
+import classes from "./Footer.module.css";
+
+const links = [
+  { link: "#", label: "Contact" },
+  { link: "#", label: "Privacy" },
+  { link: "#", label: "Blog" },
+  { link: "#", label: "Careers" },
+];
+
+export function Footer() {
+  const items = links.map((link) => (
+    <Anchor<"a">
+      c="dimmed"
+      key={link.label}
+      href={link.link}
+      onClick={(event) => event.preventDefault()}
+      size="sm"
+    >
+      {link.label}
+    </Anchor>
+  ));
+
+  return (
+    <div className={classes.footer}>
+      <Container className={classes.inner}>
+        <Image src="/favicon.png" alt="Logo" width={60} height={60} />
+        <Group className={classes.links}>{items}</Group>
+      </Container>
+    </div>
+  );
+}
