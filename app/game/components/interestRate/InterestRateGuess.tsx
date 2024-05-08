8fe66b37715e20952f829d432b253fe0b5a2a108
tweezerchef <ltomblock@gmail.com>
2024-05-07 21:41:45 -0500

    tuesday night

--- app/game/components/interestRate/InterestRateGuess.tsx
+++ app/game/components/interestRate/InterestRateGuess.tsx
@@ -3,14 +3,15 @@ import { Stack, NumberInput } from '@mantine/core'
 export function InterestRateGuess() {
   return (
     <Stack
+      style={{ height: '75%' }}
       bg="var(--mantine-color-body)"
       align="center"
       justify="center"
       gap="sm"
     >
-      <NumberInput decimalScale={3} fixedDecimalScale defaultValue={1.123} />
-      <NumberInput decimalScale={3} fixedDecimalScale defaultValue={1.123} />
-      <NumberInput decimalScale={3} fixedDecimalScale defaultValue={1.123} />
+      <NumberInput decimalScale={3} fixedDecimalScale />
+      <NumberInput decimalScale={3} fixedDecimalScale />
+      <NumberInput decimalScale={3} fixedDecimalScale />
     </Stack>
   )
 }
