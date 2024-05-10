799a29f6caea7af9c369b8bb17309a5a4d79eba9
tweezerchef <ltomblock@gmail.com>
2024-05-10 15:13:26 -0500

    updated file structure

--- app/game/interestRate/api/route.ts
+++ app/game/interestRate/api/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { arrowDecider } from '../../../../utility/arrowDecider'
+import { arrowDecider } from '../../../lib/arrowDecider'
 
 
 
