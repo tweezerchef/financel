59c443aeb388f50822997ccdf89ff07372eb65bd
tweezerchef <ltomblock@gmail.com>
2024-05-23 14:56:58 -0500

    changed burger to temp logo, fixed menu issues when opened

--- app/game/components/layout/Navbar.tsx
+++ app/game/components/layout/Navbar.tsx
@@ -1,21 +1,39 @@
 'use client'
 
-import { Burger, Group, UnstyledButton } from '@mantine/core'
+import { Group, UnstyledButton } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
+import Image from 'next/image'
 import classes from './ui/Navbar.module.css'
 
 export function Navbar() {
   const [opened, { toggle }] = useDisclosure()
 
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault() // Prevent default action for space key
+      toggle()
+    }
+  }
+
   return (
-    <div>
-      <Burger
-        className={classes.burger}
-        aria-label="Toggle navigation"
-        opened={opened}
+    <div className={classes.navbarContainer}>
+      <div
+        className={classes.logoContainer}
         onClick={toggle}
-        size="md"
-      />
+        onKeyDown={handleKeyDown}
+        role="button"
+        tabIndex={0}
+        aria-expanded={opened}
+        aria-label="Toggle navigation menu"
+      >
+        <Image
+          src="/favicon.png" // Replace with the actual logo source
+          alt="Financel Logo"
+          width={50} // Set the appropriate width
+          height={50} // Set the appropriate height
+        />
+        <span className={classes.title}>Financel</span>
+      </div>
       <Group
         className={opened ? classes.menuVisible : classes.menuHidden}
         style={{
