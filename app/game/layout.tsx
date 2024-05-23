0cbb93cf17c5bc58a91b3953b6d305beca43e8e9
tweezerchef <ltomblock@gmail.com>
2024-05-23 14:56:58 -0500

    changed burger to temp logo, fixed menu issues when opened

--- app/game/layout.tsx
+++ app/game/layout.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { Container } from '@mantine/core'
 import { Footer } from './components/layout/Footer'
 import classes from './ui/Layout.module.css'
@@ -12,15 +11,6 @@ export default function Layout({
   return (
     <Container className={classes.container}>
       <div className={classes.header}>
-        <div className={classes.logoContainer}>
-          <Image
-            src="/favicon.png" // Replace with the actual logo source
-            alt="Financel Logo"
-            width={50} // Set the appropriate width
-            height={50} // Set the appropriate height
-          />
-          <span className={classes.title}>Financel</span>
-        </div>
         <Navbar />
       </div>
       <div className={classes.mainContent}>{children}</div>
