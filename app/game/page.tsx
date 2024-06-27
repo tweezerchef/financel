8ab40568cf109ddfb872a2d8969b59e05b104a42
tweezerchef <ltomblock@gmail.com>
2024-06-27 12:30:26 +0900

    registration and login working, site protected against unautherized users.

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,15 +1,23 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import { useLocalStorage } from './lib/useLocalStorage'
 import { InterestRateGuess } from './components/interestRate/InterestRateGuess'
 import { InterestRateDayOf } from './components/interestRate/InterestRateDayOf'
 import classes from './ui/Game.module.css'
 
 export default function Game() {
+  const router = useRouter()
   const token = useLocalStorage('token')
 
-  if (token === null) return <div>Loading...</div>
-  if (!token) return <div>You are not authenticated</div>
+  if (token === null) {
+    router.push('/')
+    return <div>You are not authenticated</div>
+  }
+  if (!token) {
+    router.push('/')
+    return <div>You are not authenticated</div>
+  }
 
   return (
     <div className={classes.container}>
