89ad3a83799f5b2fc4b20a49efbd42d392c4dffe
tweezerchef <ltomblock@gmail.com>
2024-09-25 16:33:56 -0500

    primsma optimize installed, created function to create daily challenge for testing and only uses interest rate table

--- app/lib/prisma/prisma.ts
+++ app/lib/prisma/prisma.ts
@@ -1,12 +1,23 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable no-var */
 /* eslint-disable vars-on-top */
 import { PrismaClient } from '@prisma/client'
+import { withOptimize } from '@prisma/extension-optimize'
 
 declare global {
-  var prisma: PrismaClient | undefined
+  var prisma: ReturnType<typeof createPrismaClient> | undefined
 }
 
-const prisma = global.prisma || new PrismaClient()
+function createPrismaClient() {
+  if (!process.env.OPTIMIZE_API_KEY)
+    throw new Error('OPTIMIZE_API_KEY environment variable is not set')
+
+  return new PrismaClient().$extends(
+    withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY })
+  )
+}
+
+const prisma = global.prisma || createPrismaClient()
 
 if (process.env.NODE_ENV === 'development') global.prisma = prisma
 
