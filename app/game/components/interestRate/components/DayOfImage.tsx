b41ca8e2c8e713cfa137debe0fdff520100d2156
tweezerchef <ltomblock@gmail.com>
2024-09-29 15:47:36 -0500

    Transition needs work but working

--- app/game/components/interestRate/components/DayOfImage.tsx
+++ app/game/components/interestRate/components/DayOfImage.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image'
+import classes from './ui/DayOfImage.module.css'
 
 export function DayOfImage() {
   return (
-    <Image
-      src="/JP.webp"
-      alt="Interest Rate"
-      width={250}
-      height={166.5}
-      quality={80}
-      priority
-    />
+    <div className={classes.imageWrapper}>
+      <Image
+        src="/JP.webp"
+        alt="Interest Rate"
+        quality={80}
+        width={250}
+        height={166.5}
+        style={{ objectFit: 'contain' }}
+        priority
+      />
+    </div>
   )
 }
