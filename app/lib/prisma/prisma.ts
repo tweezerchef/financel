1be50ae81fec6958f6f0d4bee947422d2cb5d776
tweezerchef <ltomblock@gmail.com>
2024-09-29 16:29:05 -0500

    changes to prisma client

--- app/lib/prisma/prisma.ts
+++ app/lib/prisma/prisma.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-use-before-define */
+/* eslint-disable no-var */
 /* eslint-disable vars-on-top */
-import { PrismaClient, Prisma } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 import { withOptimize } from '@prisma/extension-optimize'
 
 declare global {
-  // eslint-disable-next-line no-var
   var prisma: ReturnType<typeof createPrismaClient> | undefined
 }
 
@@ -12,43 +12,13 @@ function createPrismaClient() {
   if (!process.env.OPTIMIZE_API_KEY)
     throw new Error('OPTIMIZE_API_KEY environment variable is not set')
 
-  const logQuery = (e: Prisma.QueryEvent) => {
-    console.log(`Query: ${e.query}`)
-    console.log(`Params: ${e.params}`)
-    console.log(`Duration: ${e.duration}ms`)
-  }
-
-  const prismaClientOptions: Prisma.PrismaClientOptions = {
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-    log:
-      process.env.NODE_ENV === 'development'
-        ? [
-            { emit: 'event', level: 'query' },
-            { emit: 'stdout', level: 'error' },
-            { emit: 'stdout', level: 'info' },
-            { emit: 'stdout', level: 'warn' },
-          ]
-        : undefined,
-  }
-
-  const baseClient = new PrismaClient(prismaClientOptions)
-
-  if (process.env.NODE_ENV === 'development')
-    baseClient.$on('query' as never, logQuery)
-
-  const prisma = baseClient.$extends(
+  return new PrismaClient().$extends(
     withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY })
   )
-
-  return prisma
 }
 
 const prisma = global.prisma || createPrismaClient()
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+if (process.env.NODE_ENV === 'development') global.prisma = prisma
 
 export default prisma
