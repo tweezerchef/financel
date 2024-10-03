90a8e48cc5d7533ccb8f861c70c0d12432f80901
tweezerchef <ltomblock@gmail.com>
2024-10-03 16:05:25 -0500

    updated login auth route to reflect changes in guess

--- app/game/dev/page.tsx
+++ app/game/dev/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { NextModal } from './components/NextModal'
 
 interface IRmodalProps {
--- app/game/interestRate/api/guess/route.ts
+++ app/game/interestRate/api/guess/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { Decimal } from '@prisma/client/runtime/library'
-import { arrowDecider } from '../../../../../lib/interestRate/arrowDecider'
-import prisma from '../../../../../lib/prisma/prisma'
+import { arrowDecider } from '../../../../lib/interestRate/arrowDecider'
+import prisma from '../../../../lib/prisma/prisma'
 
 type DailyChallenge = {
   challengeDate: Date
