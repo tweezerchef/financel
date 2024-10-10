039dc5726c62e90002cbfbcc76f681f42e70cec4
tweezerchef <ltomblock@gmail.com>
2024-10-10 11:40:57 -0500

    colorChange

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/jsx-no-duplicate-props */
 import type { Metadata } from 'next'
+import localFont from 'next/font/local'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import {
   Center,
@@ -24,7 +26,10 @@ const myColor: MantineColorsTuple = [
   '#0090cd',
   '#007cb5',
 ]
-
+const tickerFont = localFont({
+  src: './SUBWT___.ttf',
+  display: 'swap',
+})
 const theme = createTheme({
   colors: {
     myColor,
@@ -47,7 +52,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={classes.html}>
+    <html lang="en" className={`${classes.html} ${tickerFont.className}`}>
       <head>
         <ColorSchemeScript />
       </head>
