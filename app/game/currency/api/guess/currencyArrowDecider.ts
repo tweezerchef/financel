b4506ad0cfe1f04220ef79f80080da0ebd803248
tweezerchef <ltomblock@gmail.com>
2024-10-12 18:50:13 -0500

    currency arrow decider function done, range and chart values returning correctly

--- app/game/currency/api/guess/currencyArrowDecider.ts
+++ app/game/currency/api/guess/currencyArrowDecider.ts
@@ -12,25 +12,27 @@ function calculateArrowAmount(
   difference: number
   range: number
 } {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const yearRange = range || 1
+  // Calculate the percentage difference based on the range
+  const percentageDifference = (difference / range) * 100
+
   let arrows: ResponseNumbers
   let newDifference: number
-  if (difference <= 0.25) {
+
+  if (percentageDifference <= 5) {
     arrows = 1
-    newDifference = 0.25
-  } else if (difference <= 0.75) {
+    newDifference = 0.05 * range
+  } else if (percentageDifference <= 15) {
     arrows = 2
-    newDifference = 0.75
-  } else if (difference <= 1.25) {
+    newDifference = 0.15 * range
+  } else if (percentageDifference <= 25) {
     arrows = 3
-    newDifference = 1.25
-  } else if (difference <= 2.5) {
+    newDifference = 0.25 * range
+  } else if (percentageDifference <= 50) {
     arrows = 4
-    newDifference = 2.5
+    newDifference = 0.5 * range
   } else {
     arrows = 5
-    newDifference = 2.51
+    newDifference = 0.51 * range
   }
 
   return { arrows, difference: newDifference, range }
