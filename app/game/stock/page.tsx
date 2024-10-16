ea4bcedc4ee540b264d91b4792899cd3b0a1e77e
tweezerchef <ltomblock@gmail.com>
2024-10-16 15:14:16 -0500

    stockFE and backEnd mostly working need to work out some kinks

--- app/game/stock/page.tsx
+++ app/game/stock/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { StockDayOf } from './components/StockDayOf'
 import { StockGuess } from './components/StockGuess'
-import { useDailyChallengeContext } from '../../context/dailyChallenge/DailyChallengeContext'
+
 import classes from './ui/StockPage.module.css'
 
 type DayOf = 'image' | 'day'
@@ -12,14 +12,6 @@ export default function Stock() {
   const [challengeDate] = useState<DayOf>('image')
   const [amountAway, setAmountAway] = useState<number | null>(null)
   const [guessCount, setGuessCount] = useState<number | null>(null)
-  const { dailyChallengeStock, fetchDailyChallenge } =
-    useDailyChallengeContext()
-
-  useEffect(() => {
-    if (!dailyChallengeStock) fetchDailyChallenge()
-  }, [dailyChallengeStock, fetchDailyChallenge])
-
-  if (!dailyChallengeStock) return <div>Loading...</div>
 
   return (
     <div className={classes.container}>
