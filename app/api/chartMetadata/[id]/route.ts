23fed17d4aeff2edad61a1a1f405b4c666a3f27d
tweezerchef <ltomblock@gmail.com>
2024-10-26 18:32:21 -0500

    twiter share test v3

--- app/api/chartMetadata/[id]/route.ts
+++ app/api/chartMetadata/[id]/route.ts
@@ -1,7 +1,10 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(context: { params: { id: string } }) {
-  const { id } = context.params
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params
   const chartImageUrl = `https://${process.env.SERVER_AWS_S3_BUCKET_NAME}.s3.${process.env.SERVER_AWS_REGION}.amazonaws.com/chart/${id}`
 
   const metadata = {
