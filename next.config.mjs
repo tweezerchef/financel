3503848016d0d72cfce4a8aef6d1382e588283c6
tweezerchef <ltomblock@gmail.com>
2024-10-28 10:31:48 -0500

    basic basic leaderboard done

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   // change when docker for container for target
   // target: '??',
+  images: {
+    domains: [
+      'localhost',
+      's3.amazonaws.com',
+      'financle.s3.us-east-2.amazonaws.com',
+    ],
+  },
   reactStrictMode: true,
   experimental: {
     reactCompiler: true,
