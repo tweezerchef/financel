8e8d093d04f32c514e0462c75f13478f88de42cf
tweezerchef <ltomblock@gmail.com>
2024-10-28 12:49:14 -0500

    improved slightly leaderboard

--- app/game/final/page.tsx
+++ app/game/final/page.tsx
@@ -1,11 +1,14 @@
+import { Center } from '@mantine/core'
 import { ScoreChart } from './components/ScoreChart'
-import { Leaderboard } from './components/Leaderboard'
+import { LeaderBoard } from './components/LeaderBoard'
 
 export default function Final() {
   return (
     <div>
       <ScoreChart />
-      <Leaderboard />
+      <Center>
+        <LeaderBoard />
+      </Center>
     </div>
   )
 }
