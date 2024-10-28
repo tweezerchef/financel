f7ac00b597a5effe29ce6cfa4cef95e47f621f46
tweezerchef <ltomblock@gmail.com>
2024-10-28 14:52:28 -0500

    cant get UI right in Final Page

--- app/game/final/components/Leaderboard.tsx
+++ app/game/final/components/Leaderboard.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Avatar, Table, Group, Text } from '@mantine/core'
+import { Avatar, Table, Group, Text, Stack } from '@mantine/core'
 // import {
 //   IconPencil,
 //   IconMessages,
@@ -10,10 +10,31 @@ import { Avatar, Table, Group, Text } from '@mantine/core'
 //   IconDots,
 // } from '@tabler/icons-react'
 import { useLeaderboard } from '../lib/useLeaderBoard'
+import classes from './ui/LeaderBoard.module.css'
 
 export function LeaderBoard() {
   const { data } = useLeaderboard()
-  const rows = data?.topEntries.map((player) => (
+
+  const toprows = data?.topEntries.map((player) => (
+    <Table.Tr key={player.rank}>
+      <Table.Td>
+        <Group gap="sm">
+          <Text fz="sm" fw={500}>
+            {player.rank}.
+          </Text>
+          <Text fz="sm">{player.score}</Text>
+          <Avatar size={40} src={player.avatar} radius={40} />
+          <div>
+            <Text fz="sm" fw={500}>
+              {player.username}
+            </Text>
+          </div>
+        </Group>
+      </Table.Td>
+    </Table.Tr>
+  ))
+
+  const surroundingrows = data?.surroundingEntries.map((player) => (
     <Table.Tr key={player.rank}>
       <Table.Td>
         <Group gap="sm">
@@ -33,8 +54,16 @@ export function LeaderBoard() {
   ))
 
   return (
-    <Table verticalSpacing="md">
-      <Table.Tbody>{rows}</Table.Tbody>
-    </Table>
+    <Stack className={classes.stack}>
+      <Text fz="md" fw={500} align="center">
+        Leaderboard
+      </Text>
+      <div className={classes.tableWrapper}>
+        <Table verticalSpacing="xs" className={classes.table}>
+          <Table.Tbody>{toprows}</Table.Tbody>
+          <Table.Tbody>{surroundingrows}</Table.Tbody>
+        </Table>
+      </div>
+    </Stack>
   )
 }
--- app/game/final/page.tsx
+++ app/game/final/page.tsx
@@ -1,14 +1,21 @@
-import { Center } from '@mantine/core'
+'use client'
+
+// import { Center } from '@mantine/core'
+
+import { Stack } from '@mantine/core'
 import { ScoreChart } from './components/ScoreChart'
 import { LeaderBoard } from './components/LeaderBoard'
+import classes from './ui/Page.module.css'
 
 export default function Final() {
   return (
-    <div>
-      <ScoreChart />
-      <Center>
-        <LeaderBoard />
-      </Center>
+    <div className={classes.container}>
+      <div className={classes.content}>
+        <Stack style={{ flex: 1, minHeight: 0 }}>
+          <ScoreChart />
+          <LeaderBoard />
+        </Stack>
+      </div>
     </div>
   )
 }
