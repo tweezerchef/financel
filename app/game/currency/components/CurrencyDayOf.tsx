5430fd8878a2db43c60d6dc9afa796b626662bde
tweezerchef <ltomblock@gmail.com>
2024-11-04 17:44:08 -0600

    updated banner to all components

--- app/game/currency/components/CurrencyDayOf.tsx
+++ app/game/currency/components/CurrencyDayOf.tsx
@@ -87,7 +87,10 @@ export function CurrencyDayOf({ amountAway, guessCount }: CurrencyDayOfProps) {
           )}
         </Transition>
       </div>
-      <div className={classes.legend} ref={legendRef}>
+      <div
+        className={`${classes.legend} ${dayOfSlide === 'day' ? classes.legendVisible : ''}`}
+        ref={legendRef}
+      >
         <div
           className={`${classes.legendContent} ${isAnimating ? classes.animate : ''}`}
         >
