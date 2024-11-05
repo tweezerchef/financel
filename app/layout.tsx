af72c83f8a5f8b550b43385e11531e419050802a
tweezerchef <ltomblock@gmail.com>
2024-11-05 17:36:56 -0600

    changed speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-/* eslint-disable react/jsx-no-duplicate-props */
-
 import type { Metadata } from 'next'
 import LocalFont from 'next/font/local'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { Analytics } from '@vercel/analytics/react'
 import {
   Center,
